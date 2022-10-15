f0900dbcf4c0cd112c0d82bf2475b9ba66af0846
santigarcia1 <69224052+santigarcia1@users.noreply.github.com>
2022-10-15 19:41:36 -0300

    add dependencies stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
-import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -10,33 +10,34 @@ import { ArticulosFamiliasComponent } from './components/articulos-familias/arti
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
-import { ArticulosComponent } from './components/articulos/articulos.component';  
+import { ArticulosComponent } from './components/articulos/articulos.component';
 import { CortarCantidadCaracteres } from './pipes/custom-pipe.pipe';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
-  imports: [ 
-    BrowserModule, 
-    FormsModule, 
+  imports: [
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
       { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
-      { path: 'articulos', component: ArticulosComponent }
+      { path: 'articulos', component: ArticulosComponent },
     ]),
-    NgbModule
+    NgbModule,
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    HelloComponent, 
-    InicioComponent, 
-    ArticulosFamiliasComponent, 
-    MenuComponent, 
-    ArticulosComponent, 
-    CortarCantidadCaracteres 
+    HelloComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+    MenuComponent,
+    ArticulosComponent,
+    CortarCantidadCaracteres,
   ],
-  bootstrap:    [ AppComponent ],
-  providers: [ { provide: APP_BASE_HREF, useValue: "/" } ]
+  bootstrap: [AppComponent],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
-export class AppModule { }
+export class AppModule {}
